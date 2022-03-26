bf7e955ea3dfb52163a28af0ae49cc974f10811a
sakshamkadayat <sakshamkadayat@gmail.com>
2022-03-25 12:47:23 -1200

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import ReactDOM from 'react-dom';
 import React  from 'react';
-ReactDOM.render(
-  <h1>This is Testing</h1>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <h1>This is Testing</h1>,
+//   document.getElementById('root')
+// );
+
+
+//Introducing JSX
+const name= 'IT.Chandra';
+const element= <h1>Hello , {name}</h1>
+
+ReactDOM.render(element,document.getElementById('root'))
